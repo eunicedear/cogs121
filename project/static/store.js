567e71a27f3caa8b6ff5ab4f8a1bb89eb6ea33e8
Kellie <kkhiga@ucsd.edu>
2018-06-04 05:00:31 -0700

    Store

--- project/static/store.js
+++ project/static/store.js
@@ -10,15 +10,67 @@ var config = {
 
 firebase.initializeApp(config);
 
+function selectedAccessory(element) {
+  console.log('element ', element);
+  var accessoryid = element.getAttribute("accessoryid");
+  console.log('accessory ', accessoryid, ' clicked');
+  localStorage.setItem('accessoryid', accessoryid);
+}
+
+function getPoints(userid, childid) {
+  var data;
+  database.ref('users/' + userid + '/children/' + childid + '/points').once('value', (snapshot) => {
+    data = snapshot.val();
+    console.log('userid = ' + userid + ' childid = ' + childid);
+    console.log('data = ' + data);
+    points = data;
+    console.log('points = ', points);
+    $("#ptsTotal").text(points + " pts");
+  })
+}
+
+function displayStore() {
+  database.ref('accessories').orderByKey().once('value', snapshot => {
+    const accessories = snapshot.val();
+    console.log(accessories);
+    if (accessories) {
+      console.log('displayStore()');
+      snapshot.forEach((itemSnapshot) => {
+        console.log('item', itemSnapshot);
+        
+
+        // var key = element.key;
+        // var accessoryid = element.val();
+        // var accessoryurl = database.ref('accessories/' + key + '/itemURL');
+        // console.log('accessoryid = ' + accessoryid);
+        // console.log('accessoryurl = ' + accessoryurl);
+        //
+        // var selectedTemplate = $('#item-placeholder');
+        // console.log('selectedTemplate ' + selectedTemplate.text);
+        // var selectedImage = $("#accessory-img");
+        // console.log('selectedImage ' + selectedImage.html);
+        // selectedImage.attr("src", 'assets/accessories/' + accessoryurl);
+        // var wrapper = $('.wrapper');
+        //
+        // selectedImage.clone().appendTo(wrapper);
+      });
+    }
+  })
+}
+
 function initApp() {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-			// User is signed in.
-			console.log( 'user.uid = ' + user.uid );
-			// var userid;
-      localStorage.setItem( 'userid', user.uid );
+      // User is signed in.
+      console.log('user.uid = ' + user.uid);
+      // var userid;
+      localStorage.setItem('userid', user.uid);
       database = firebase.database();
       // viewChildren();
+      userid = localStorage.getItem('userid');
+      childid = localStorage.getItem('childid');
+      getPoints(userid, childid);
+      displayStore();
     } else {
       // User is signed out.
       console.log("User is signed out.");
@@ -29,84 +81,38 @@ function initApp() {
   });
 };
 
-function selectedAccessory( element ) {
-	console.log('element ', element);
-	var accessoryid = element.getAttribute( "accessoryid" );
-	console.log( 'accessory ', accessoryid, ' clicked');
-	localStorage.setItem( 'accessoryid', accessoryid );
-}
-
-function getPoints( userid, childid ) {
-	var data;
-	database.ref( 'users/' + userid + '/children/' + childid + '/points' ).once( 'value', ( snapshot ) => {
-		data = snapshot.val();
-		console.log( 'userid = ' + userid + ' childid = ' + childid );
-		console.log( 'data = ' + data );
-		points = data;
-		console.log( 'points = ', points );
-		$("#ptsTotal").text(points + " pts");
-	})
-}
-
-function displayStore() {
-	database.ref( 'accessories' ).orderByKey().once( 'value', snapshot => {
-		const accessories = snapshot.val();
-		console.log( accessories );
-		if( accessories ) {
-			console.log( 'displayStore()' );
-			snapshot.forEach( element => {
-				var key = element.key;
-				var accessoryid = element.val();
-				var accessoryurl = database.ref( 'accessories/' + key + '/itemURL' );
-				console.log( 'accessoryid = ' + accessoryid );
-				console.log( 'accessoryurl = ' + accessoryurl );
-
-				var selectedClass = document.querySelector( '#item-placeholder' );
-				console.log( 'selectedClass ' + selectedClass );
-				var selectedAttr = selectedClass.querySelector( "#accessory-img" );
-				console.log( 'selectedAttr ' + selectedAttr );
-				selectedAttr.setAttribute( "src", 'assets/accessories/' + accessoryurl );
-
-        // var clone = document.importNode(selectedClass.content, true);
-			});
-		}
-	} )
-}
-
 window.addEventListener('load', function() {
   initApp();
+
+  console.log('points = ' + points);
 });
 
 
 $(document).ready(() => {
-	database = firebase.database();
-	initApp();
-	console.log( 'pg loaded' );
-
-	displayStore();
-	
-	var userid = localStorage.getItem( 'userid' );
-	console.log( '56: userid = ' + userid );
-	var childid = localStorage.getItem( 'childid' );
-	getPoints( userid, childid );
-	console.log( 'points = ' + points );
-
-
-	
-	
+  // database = firebase.database();
+  // initApp();
+  // console.log('pg loaded');
+  //
+  // displayStore();
+  //
+  // var userid = localStorage.getItem('userid');
+  // console.log('56: userid = ' + userid);
+  // var childid = localStorage.getItem('childid');
+  // getPoints(userid, childid);
+  // console.log('points = ' + points);
 
   // upon clicked accessory, save id to local storage
-  $('.items').click( () => {
-  	var accessoryid = this.getAttribute( "accessoryid" );
-  	localStorage.setItem( 'accessoryid', accessoryid );
-
-  	$.ajax( {
-  		url: 'toPurchase',
-  		type: 'POST',
-  		data: {
-
-  		}
-  	})
-  })
+  // $('.items').click(() => {
+  //   var accessoryid = this.getAttribute("accessoryid");
+  //   localStorage.setItem('accessoryid', accessoryid);
+  //
+  //   // $.ajax({
+  //   //   url: 'toPurchase',
+  //   //   type: 'POST',
+  //   //   data: {
+  //   //
+  //   //   }
+  //   // })
+  // })
 
 });
